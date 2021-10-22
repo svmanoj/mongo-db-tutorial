cc5a598dd79c6b26b510fa94bf88069490c806fc
Manoj Test <manojtest@Manojs-MacBook-Pro.local>
2021-10-22 22:22:06 +0530

    Support for cast based search

--- mflix/src/main/java/mflix/api/daos/MovieDao.java
+++ mflix/src/main/java/mflix/api/daos/MovieDao.java
@@ -167,8 +167,8 @@ public class MovieDao extends AbstractMFlixDao {
      * @return List of documents sorted by sortKey that match the cast selector.
      */
     public List<Document> getMoviesByCast(String sortKey, int limit, int skip, String... cast) {
-        Bson castFilter = null;
-        Bson sort = null;
+        Bson castFilter = in("cast", cast);
+        Bson sort = new Document(sortKey, -1);
         //TODO> Ticket: Subfield Text Search - implement the expected cast
         // filter and sort
         List<Document> movies = new ArrayList<>();
